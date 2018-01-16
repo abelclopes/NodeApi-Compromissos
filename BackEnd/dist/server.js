f640857e43daa9911c4007b472dbe3be04257a14
Abel Lopes <abellopes@gmail.com>
2018-01-16 15:56:51 +0000

    esta rodando

--- BackEnd/dist/server.js
+++ BackEnd/dist/server.js
@@ -1,18 +1,23 @@
-var jsonServer = require('json-server');
+"use strict";
+exports.__esModule = true;
+var jsonServer = require("json-server");
+var fs = require("fs");
+var https = require("https");
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
-// Add custom routes before JSON Server router
-server.get('/echo', function (req, res) {
-    res.jsonp(req.query);
-});
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
+//middleware para login
 // Use default router
 server.use(router);
-server.listen(3000, function () {
-    console.log('JSON Server is running');
+var options = {
+    cert: fs.readFileSync('./BackEnd/keys/cert.pem'),
+    key: fs.readFileSync('./BackEnd/keys/key.pem')
+};
+https.createServer(options, server).listen(3001, function () {
+    console.log('JSON Server is running on https://localhost:3001');
 });
